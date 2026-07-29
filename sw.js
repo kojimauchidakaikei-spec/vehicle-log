cd863e6497a0c1e014e227462bebc152b17f988e
kojimauchidakaikei-spec <kojima.uchidakaikei@gmail.com>
2026-07-29 13:49:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,41 @@
-const CACHE='vehicle-log-v1.0.0';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE = 'vehicle-log-v1.1.0';
 
-self.addEventListener('install',e=>
-  e.waitUntil(
-    caches.open(CACHE)
-      .then(c=>c.addAll(ASSETS))
-      .then(()=>self.skipWaiting())
-  )
-);
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-self.addEventListener('activate',e=>
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(
-        keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))
-      ))
-      .then(()=>self.clients.claim())
-  )
-);
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener('fetch',e=>
-  e.respondWith(
-    caches.match(e.request)
-      .then(r=>r||fetch(e.request))
-  )
-);
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
